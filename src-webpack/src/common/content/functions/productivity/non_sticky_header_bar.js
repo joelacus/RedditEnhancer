0de7c9dc846a01426395706fb0b564639c9fcdd1
Bùi Minh Đức <93396463+bmndc@users.noreply.github.com>
2024-12-17 15:00:41 +0700

    fix: target the correct elements

--- src-webpack/src/common/content/functions/productivity/non_sticky_header_bar.js
+++ src-webpack/src/common/content/functions/productivity/non_sticky_header_bar.js
@@ -52,12 +52,12 @@ function enableNonStickyHeaderBarNewNew() {
 	styleElement.textContent = `shreddit-app reddit-header-large {
 									position: absolute !important;
 								}
-								shreddit-app reddit-sidebar-nav {
+								shreddit-app reddit-sidebar-nav#left-sidebar {
 									position: sticky;
 									height: 100vh;
 									top: 0;
 								}
-								shreddit-app .right-sidebar-container {
+								shreddit-app #right-sidebar-container {
 									position: sticky;
 									height: 100vh;
 									top: 0;
