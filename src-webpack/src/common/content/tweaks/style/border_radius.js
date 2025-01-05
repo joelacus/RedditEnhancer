fec552b6e2f80a3a271b06f4978e2f6ea58788b8
Bùi Minh Đức <93396463+bmndc@users.noreply.github.com>
2025-01-05 11:44:11 +0700

    refactor: Minor changes to Theme Corner Radius
    * Theme Border Radius now only targets elements known to be outside shadow DOMs instead of blindly targeting all
    * Theme Border Radius now applies to comment search results in post view

--- src-webpack/src/common/content/tweaks/style/border_radius.js
+++ src-webpack/src/common/content/tweaks/style/border_radius.js
@@ -29,20 +29,58 @@ function addBorderRadiusAmountStylesheet() {
 	if (!document.head.querySelector('style[id="re-theme-border-radius"]')) {
 		const styleElement = document.createElement('style');
 		styleElement.id = 're-theme-border-radius';
-		styleElement.textContent = `.xs\\:rounded-\\[16px\\],
-									.rounded-\\[8px\\],
-									.rounded-\\[1rem\\],
-									comment-body-header,
-									shreddit-comment-tree,
-									shreddit-post .hover-card,
-									.rounded-t-\\[1rem\\] {
-										border-radius: var(--re-theme-border-radius) !important;
-									}
-									.rounded-\\[16px\\],
-									:where(button), :where(input):where([type="submit"], [type="reset"], [type="button"]),
-									.button {
-										border-radius: calc(var(--re-theme-border-radius) / 2) !important;
-									}`;
+		styleElement.textContent =
+			`/* Posts, search results and subreddit sidebar */
+			shreddit-app article > shreddit-post,
+			shreddit-post div.hover-card,
+			main#main-content search-telemetry-tracker > div:not([data-testid="search-scope-switcher"]),
+			div[data-testid="search-crosspost-unit"] div:has(> search-telemetry-tracker),
+			div#right-sidebar-container > *,
+			div#right-sidebar-container aside a,
+			div#right-sidebar-container aside button,
+			/* Post media previews */
+			div[slot="thumbnail"],
+			div[slot="post-media-container"],
+			div[slot="post-media-container"] > div,
+			gallery-carousel li:has(img.media-lightbox-img),
+			div:has(> shreddit-aspect-ratio),
+			/* Comment search results */
+			reddit-feed > faceplate-tracker > div,
+			faceplate-tracker[noun="load_more_comments"] button,
+			/* Wiki pages */
+			shreddit-app[routename="subreddit_wiki"] main#main-content > div:last-of-type,
+			div.toc {
+				border-radius: var(--re-theme-border-radius) !important;
+			}
+			faceplate-tracker[noun="load_more_comments"] button {
+				position: relative;
+			}
+			/* Post flairs */
+			shreddit-post-flair[slot="post-flair"] span,
+			author-flair-event-handler span,
+			community-achievements-flair span,
+			div.flair span,
+			span:has(div.flair-content) {
+				line-height: 1.4 !important;
+				border-radius: calc(var(--re-theme-border-radius) / 2) !important;
+			}
+			/* Keep some space between post flair and content */
+			shreddit-app[routename="post_page"] div[slot="text-body"],
+			shreddit-app[routename="post_page"] div[slot="post-media-container"] {
+				margin-top: .5rem;
+			}
+			/* Notification panel */
+			div[data-id="notification-container-element"] > div:first-of-type {
+				border-radius: var(--re-theme-border-radius) var(--re-theme-border-radius) 0 0;
+				background-color: var(--color-neutral-background-container);
+			}
+			div[data-id="notification-container-element"] > div:last-of-type {
+				padding: 0;
+
+				& > faceplate-tracker a {
+					border-radius: 0 0 var(--re-theme-border-radius) var(--re-theme-border-radius);
+				}
+			}`;
 		document.head.insertBefore(styleElement, document.head.firstChild);
 	}
 }
