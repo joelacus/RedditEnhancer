656ecb84e93d389f5cf357f4fd5f6cc3544cf200
Joelacus <jnbutcher941@gmail.com>
2025-07-04 02:56:35 +0100

    feat: "Auto Expand Comments" - It now remembers if a user has closed a comment thread and will not re-expand it. - Removed support for "Old New UI"

--- src-webpack/src/common/content/tweaks/productivity/auto_expand_comments.js
+++ src-webpack/src/common/content/tweaks/productivity/auto_expand_comments.js
@@ -1,4 +1,12 @@
-/* ===== Tweaks - Productivity - Auto Expand Comments ===== */
+/**
+ * Tweaks: Productivity - Auto Expand Comments
+ *
+ * @name autoExpandComments
+ * @description Automatically expand collapsed comments as the user scrolls down the post comments page.
+ *
+ * Applies to: Old UI (2005-)
+ *             New New UI (2023-)
+ */
 
 /* === Triggered On Page Load === */
 export function loadAutoExpandComments() {
@@ -9,34 +17,26 @@ export function loadAutoExpandComments() {
 
 /* === Main Function === */
 export function autoExpandComments(value) {
-	if (redditVersion === 'newnew' && value === true) {
+	if (redditVersion === 'newnew' && value) {
 		enableAutoExpandCommentsNewNew();
-	} else if (redditVersion === 'new' && value === true) {
-		enableAutoExpandCommentsNew();
-	} else if (redditVersion === 'old' && value === true) {
+	} else if (redditVersion === 'old' && value) {
 		enableAutoExpandCommentsOld();
 	} else if (value === false) {
 		disableAutoExpandCommentsAll();
 	}
 }
 
-let expand_comments_button;
+let comment_id;
 
 // Function - Enable Auto Expand Comments - New New
 function enableAutoExpandCommentsNewNew() {
-	expand_comments_button = 'shreddit-comment[collapsed]';
-	window.addEventListener('scroll', expandComments);
-}
-
-// Function - Enable Auto Expand Comments - New
-function enableAutoExpandCommentsNew() {
-	expand_comments_button = '.Comment:has(> button > .icon-expand) > :last-child.undefined';
+	comment_id = 'shreddit-comment';
 	window.addEventListener('scroll', expandComments);
 }
 
 // Function - Enable Auto Expand Comments - Old
 function enableAutoExpandCommentsOld() {
-	expand_comments_button = '.comment.collapsed';
+	comment_id = '.comment.collapsed';
 	window.addEventListener('scroll', expandComments);
 }
 
@@ -45,48 +45,58 @@ function disableAutoExpandCommentsAll() {
 	window.removeEventListener('scroll', expandComments);
 }
 
-// Function to check for "expand comments" buttons on scroll
+// Function to check for collapsed comment threads on scroll
 function expandComments() {
 	// Get the current scroll position
-	var scrollX = window.scrollX || window.pageXOffset;
-	var scrollY = window.scrollY || window.pageYOffset;
+	const scrollX = window.scrollX || window.pageXOffset;
+	const scrollY = window.scrollY || window.pageYOffset;
 
-	// Get the elements within the current scroll view
-	var elementsInViewport = document.querySelectorAll(expand_comments_button);
-
-	var visibleElements = [];
-
-	elementsInViewport.forEach(function (element) {
-		var rect = element.getBoundingClientRect();
-		var elementX = rect.left + scrollX;
-		var elementY = rect.top + scrollY;
-
+	// Get the comments within the current scroll view
+	const comments = document.querySelectorAll(comment_id);
+	let visibleComments = [];
+	comments.forEach(function (element) {
+		const rect = element.getBoundingClientRect();
+		const elementX = rect.left + scrollX;
+		const elementY = rect.top + scrollY;
 		if (elementX >= scrollX && elementX <= scrollX + window.innerWidth && elementY >= scrollY && elementY <= scrollY + window.innerHeight) {
-			visibleElements.push(element);
+			visibleComments.push(element);
 		}
 	});
 
-	// Expand Comments
+	// Expand the comments
 	if (redditVersion === 'newnew') {
-		for (let i = 0; i < visibleElements.length; i++) {
-			if (visibleElements[i].getAttribute('author') != 'AutoModerator') {
-				visibleElements[i].removeAttribute('collapsed');
-			}
-		}
-	} else if (redditVersion === 'new') {
-		for (let i = 0; i < visibleElements.length; i++) {
-			if (!visibleElements[i].querySelector('[id^="CommentTopMeta--Mod"]')) {
-				if (window.getComputedStyle(visibleElements[i].parentElement.parentElement).display === 'block') {
-					visibleElements[i].parentElement.querySelector('button').click();
+		for (let i = 0; i < visibleComments.length; i++) {
+			if (visibleComments[i].getAttribute('author') != 'AutoModerator') {
+				// auto expand comments if not collapsed by the user
+				if (visibleComments[i].getAttribute('re-keep-collapsed') !== 'true') {
+					if (visibleComments[i].nodeName === 'SHREDDIT-COMMENT') {
+						visibleComments[i].removeAttribute('collapsed');
+					}
+					if (visibleComments[i].querySelector('[loading="action"][slot^="children"]')) {
+						setTimeout(() => {
+							visibleComments[i].querySelector('[loading="action"][slot^="children"]').click();
+						}, 100);
+					}
 				}
+
+				// add an event listener to remember if the user has collapsed a comment thread
+				setTimeout(() => {
+					if (!visibleComments[i].hasAttribute('re-keep-collapsed') && visibleComments[i].shadowRoot.querySelector('button')) {
+						visibleComments[i].setAttribute('re-keep-collapsed', false);
+						visibleComments[i].shadowRoot.querySelector('button').addEventListener('click', function (e) {
+							const keep_collapsed = visibleComments[i].getAttribute('re-keep-collapsed');
+							visibleComments[i].setAttribute('re-keep-collapsed', keep_collapsed === 'false' ? true : false);
+						});
+					}
+				}, 150);
 			}
 		}
 	} else if (redditVersion === 'old') {
-		for (let i = 0; i < visibleElements.length; i++) {
-			if (visibleElements[i].getAttribute('data-author') != 'AutoModerator') {
-				visibleElements[i].classList.remove('collapsed');
-				visibleElements[i].classList.add('noncollapsed');
-				visibleElements[i].querySelector('.expand').textContent = '[–]';
+		for (let i = 0; i < visibleComments.length; i++) {
+			if (visibleComments[i].getAttribute('data-author') != 'AutoModerator') {
+				visibleComments[i].classList.remove('collapsed');
+				visibleComments[i].classList.add('noncollapsed');
+				visibleComments[i].querySelector('.expand').textContent = '[–]';
 			}
 		}
 	}
