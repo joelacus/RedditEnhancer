3a02cbaa9fb4ae8e003b9c452d81db7ba7288b22
Joelacus <jnbutcher941@gmail.com>
2026-02-09 05:15:46 +0000

    fix: minor fixes regarding font weight

--- src-webpack/src/common/popup/inputs/inputs_font.js
+++ src-webpack/src/common/popup/inputs/inputs_font.js
@@ -94,61 +94,56 @@ document.querySelector('#input-create-post-body-font-size').addEventListener('mo
 // Slider - Post Title Font Weight
 document.querySelector('#input-post-title-font-weight').addEventListener('input', (e) => {
 	const value = e.target.value;
-	document.querySelector('.icon-post-title-font-weight').style.backgroundColor = value != 9 ? 'var(--accent)' : '';
-	document.querySelector('#post-title-font-weight-value').innerText = value != 9 ? `${e.target.value}px` : '';
+	document.querySelector('.icon-post-title-font-weight').style.backgroundColor = value != 0 ? 'var(--accent)' : '';
+	document.querySelector('#post-title-font-weight-value').innerText = value != 0 ? e.target.value : '';
 	sendMessage({ postTitleFontWeight: value });
 });
 document.querySelector('#input-post-title-font-weight').addEventListener('mouseup', (e) => {
-	const value = e.target.value !== 9 ? e.target.value : false;
-	BROWSER_API.storage.sync.set({ postTitleFontWeight: value });
+	BROWSER_API.storage.sync.set({ postTitleFontWeight: e.target.value });
 });
 
 // Slider - Post Content Font Weight
 document.querySelector('#input-post-content-font-weight').addEventListener('input', (e) => {
 	const value = e.target.value;
-	document.querySelector('.icon-post-content-font-weight').style.backgroundColor = value != 9 ? 'var(--accent)' : '';
-	document.querySelector('#post-content-font-weight-value').innerText = value != 9 ? `${e.target.value}px` : '';
+	document.querySelector('.icon-post-content-font-weight').style.backgroundColor = value != 0 ? 'var(--accent)' : '';
+	document.querySelector('#post-content-font-weight-value').innerText = value != 0 ? e.target.value : '';
 	sendMessage({ postContentFontWeight: value });
 });
 document.querySelector('#input-post-content-font-weight').addEventListener('mouseup', (e) => {
-	const value = e.target.value !== 9 ? e.target.value : false;
-	BROWSER_API.storage.sync.set({ postContentFontWeight: value });
+	BROWSER_API.storage.sync.set({ postContentFontWeight: e.target.value });
 });
 
 // Slider - Feed Post Title Font Weight
 document.querySelector('#input-feed-post-title-font-weight').addEventListener('input', (e) => {
 	const value = e.target.value;
-	document.querySelector('.icon-feed-post-title-font-weight').style.backgroundColor = value != 9 ? 'var(--accent)' : '';
-	document.querySelector('#feed-post-title-font-weight-value').innerText = value != 9 ? `${e.target.value}px` : '';
+	document.querySelector('.icon-feed-post-title-font-weight').style.backgroundColor = value != 0 ? 'var(--accent)' : '';
+	document.querySelector('#feed-post-title-font-weight-value').innerText = value != 0 ? e.target.value : '';
 	sendMessage({ feedPostTitleFontWeight: value });
 });
 document.querySelector('#input-feed-post-title-font-weight').addEventListener('mouseup', (e) => {
-	const value = e.target.value !== 9 ? e.target.value : false;
-	BROWSER_API.storage.sync.set({ feedPostTitleFontWeight: value });
+	BROWSER_API.storage.sync.set({ feedPostTitleFontWeight: e.target.value });
 });
 
 // Slider - Feed Post Content Font Weight
 document.querySelector('#input-feed-post-content-font-weight').addEventListener('input', (e) => {
 	const value = e.target.value;
-	document.querySelector('.icon-feed-post-content-font-weight').style.backgroundColor = value != 9 ? 'var(--accent)' : '';
-	document.querySelector('#feed-post-content-font-weight-value').innerText = value != 9 ? `${e.target.value}px` : '';
+	document.querySelector('.icon-feed-post-content-font-weight').style.backgroundColor = value != 0 ? 'var(--accent)' : '';
+	document.querySelector('#feed-post-content-font-weight-value').innerText = value != 0 ? e.target.value : '';
 	sendMessage({ feedPostContentFontWeight: value });
 });
 document.querySelector('#input-feed-post-content-font-weight').addEventListener('mouseup', (e) => {
-	const value = e.target.value !== 9 ? e.target.value : false;
-	BROWSER_API.storage.sync.set({ feedPostContentFontWeight: value });
+	BROWSER_API.storage.sync.set({ feedPostContentFontWeight: e.target.value });
 });
 
 // Slider - Post Comments Font Weight
 document.querySelector('#input-post-comments-font-weight').addEventListener('input', (e) => {
 	const value = e.target.value;
-	document.querySelector('.icon-post-comments-font-weight').style.backgroundColor = value != 9 ? 'var(--accent)' : '';
-	document.querySelector('#post-comments-font-weight-value').innerText = value != 9 ? `${e.target.value}px` : '';
+	document.querySelector('.icon-post-comments-font-weight').style.backgroundColor = value != 0 ? 'var(--accent)' : '';
+	document.querySelector('#post-comments-font-weight-value').innerText = value != 0 ? e.target.value : '';
 	sendMessage({ postCommentsFontWeight: value });
 });
 document.querySelector('#input-post-comments-font-weight').addEventListener('mouseup', (e) => {
-	const value = e.target.value !== 9 ? e.target.value : false;
-	BROWSER_API.storage.sync.set({ postCommentsFontWeight: value });
+	BROWSER_API.storage.sync.set({ postCommentsFontWeight: e.target.value });
 });
 
 // Toggle - Custom Fonts
